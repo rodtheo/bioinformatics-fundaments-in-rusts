a57fc269d63f8f107ab183d882a7c26eb0fa387f
rodtheo <theodoro.biotec@gmail.com>
2024-04-05 21:18:07 -0300

    deleting target dir

--- target/debug/deps/globalalignment-019f0259abf7f3b3.d
+++ target/debug/deps/globalalignment-019f0259abf7f3b3.d
@@ -1,6 +1,6 @@
-/home/rodtheo/Bioinfo/project-data/rust/advent-of-code-2021/finite-state-advent/target/debug/deps/libglobalalignment-019f0259abf7f3b3.rmeta: src/lib.rs src/scores/mod.rs src/scores/aa.rs Cargo.toml
+/home/rodtheo/Bioinfo/project-data/rust/bioinformatics-fundaments-in-rust/target/debug/deps/libglobalalignment-019f0259abf7f3b3.rmeta: src/lib.rs src/scores/mod.rs src/scores/aa.rs Cargo.toml
 
-/home/rodtheo/Bioinfo/project-data/rust/advent-of-code-2021/finite-state-advent/target/debug/deps/globalalignment-019f0259abf7f3b3.d: src/lib.rs src/scores/mod.rs src/scores/aa.rs Cargo.toml
+/home/rodtheo/Bioinfo/project-data/rust/bioinformatics-fundaments-in-rust/target/debug/deps/globalalignment-019f0259abf7f3b3.d: src/lib.rs src/scores/mod.rs src/scores/aa.rs Cargo.toml
 
 src/lib.rs:
 src/scores/mod.rs:
--- target/debug/deps/globalalignment-0a3498fe1af491a9.d
+++ target/debug/deps/globalalignment-0a3498fe1af491a9.d
@@ -1,6 +1,6 @@
-/home/rodtheo/Bioinfo/project-data/rust/advent-of-code-2021/finite-state-advent/target/debug/deps/libglobalalignment-0a3498fe1af491a9.rmeta: src/main.rs Cargo.toml
+/home/rodtheo/Bioinfo/project-data/rust/bioinformatics-fundaments-in-rust/target/debug/deps/libglobalalignment-0a3498fe1af491a9.rmeta: src/main.rs Cargo.toml
 
-/home/rodtheo/Bioinfo/project-data/rust/advent-of-code-2021/finite-state-advent/target/debug/deps/globalalignment-0a3498fe1af491a9.d: src/main.rs Cargo.toml
+/home/rodtheo/Bioinfo/project-data/rust/bioinformatics-fundaments-in-rust/target/debug/deps/globalalignment-0a3498fe1af491a9.d: src/main.rs Cargo.toml
 
 src/main.rs:
 Cargo.toml:
